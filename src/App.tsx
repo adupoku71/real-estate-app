941283a39c345f72b6d56eb0efba1b3a1d29cdbb
adupoku71 <davidpoku71@gmail.com>
2025-08-12 14:53:09 +0000

    finished up testimonials section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { About } from "./components/About";
 import { Header } from "./components/Header";
 import { Projects } from "./components/Projects";
+import { Testimonials } from "./components/Testimonials";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <About />
       <Projects />
+      <Testimonials />
     </div>
   );
 }
